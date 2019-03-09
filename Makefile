fd16c20066ebb7726fcac76d595085c5be39aa6c
pendulm <lonependulm@gmail.com>
2019-03-09 13:38:55 +0800

    automate tools

---
+++ Makefile
@@ -0,0 +1,2 @@
+%:
+	@bash scripts/golang_template.sh $@
---
+++ scripts/golang_template.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+problem_number=$1
+problem_dir=$(leetcode show $1|head -1|sed 's/[][]//g;s/^[ \t]*//;s/[ \t]*$//'|tr ' ' '-')
+src_file=${problem_dir#*-}
+src_file=$(echo ${src_file//-/_} | tr '[A-Z]' '[a-z]')
+src_file="$src_file.go"
+
+fill_golang_template() {
+    cat <<-EOF > $src_file
+package main
+
+import (
+    "fmt"
+)
+
+
+func main() {
+}
+EOF
+}
+
+main() {
+    mkdir -p $problem_dir
+    cd $problem_dir
+    if [[ ! -f $problem_dir/$src_file ]]; then
+        fill_golang_template
+        echo "create templdate $src_file"
+    fi
+}
+
+main
+
+
